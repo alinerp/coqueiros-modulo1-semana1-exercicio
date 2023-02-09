0d86771dce6b714832123cc6e694219a76f6771e
Aline Passos <passosaline91@gmail.com>
2023-02-08 21:26:22 -0300

    ajuste na concatenação

--- tabuada/Program.cs
+++ tabuada/Program.cs
@@ -10,8 +10,8 @@ Crie um programa que receba um número inteiro, e a partir desse número exiba a
 
 System.Console.WriteLine("Criando a estrutura da sua taboada");
 System.Console.WriteLine("Digite abaixo de 0 a 10:");
-int taboada = int.Parse(Console.ReadLine());
+int tabuada = int.Parse(Console.ReadLine());
 
 for(int multiplicador =  1; multiplicador <= 10; multiplicador++) {
-    Console.WriteLine(multiplicador*taboada);
-    }
+    Console.WriteLine($"{tabuada} x {multiplicador} = {multiplicador*tabuada}");
+    };
